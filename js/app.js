6b721ab84700c6486742bd180a829efa0ed203d8
jmocana <telecomu4@gmail.com>
2019-01-28 09:36:44 +0100

    solved eror with createElement and capured values of form elements

--- js/app.js
+++ js/app.js
@@ -1,13 +1,47 @@
+function Seguro(marca, anio, tipo){
+  this.marca = marca;
+  this.anio = anio;
+  this.tipo = tipo
+}
+
+function Interfaz(){
+
+}
+
 /** select with 20 last years */
 const selectAnios = document.getElementById('anio');
 
 const max = new Date().getFullYear(),
       min = max - 20;
 
+      console.log(max)
+
 for (let i = max; i > min; i--){
-  let option = createElement('option');
+  let option = document.createElement('option');
   option.value = i;
   option.innerHTML = i;
   selectAnios.appendChild(option);
 }
+
+const formularioSeguro = document.getElementById('cotizar-seguro');
+
+formularioSeguro.addEventListener('submit', function(e){
+  e.preventDefault();
+  
+  const marca = document.getElementById('marca');
+  const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+  
+  const anio = document.getElementById('anio');
+  const anioSeleccionado = anio.options[anio.selectedIndex].value;
+  
+  const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+  const interfaz = new Interfaz;
+
+  if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === ''){
+    console.log('obligatorio rellenar todos los datos')
+  }else{
+    console.log('datos correctos');
+  }
+})
 
