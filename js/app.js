499ff4c18cdd9c3a000a5f51b82733858a2a4965
jmocana <telecomu4@gmail.com>
2019-01-30 13:05:07 +0100

    finished project with proptypes and class

--- js/app.js
+++ js/app.js
@@ -1,20 +1,99 @@
-function Seguro(marca, anio, tipo){
-  this.marca = marca;
-  this.anio = anio;
-  this.tipo = tipo
+class Seguro{
+  constructor(marca, anio, tipo){
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo
+  }
+
+  cotizarSeguro(){
+  
+    let cantidad;
+    const base = 2000
+  
+    switch (this.marca){
+      case '1':{
+        cantidad = base * 1.15;
+        break;
+      }
+      
+      case '2':{
+        cantidad = base * 1.05;
+        break
+      }
+  
+      case '3':{
+        cantidad = base * 1.35;
+        break
+      }    
+    }
+  
+    const diferenciaAnyo = new Date().getFullYear() - this.anio;
+    
+    cantidad -= ((diferenciaAnyo * 3) * cantidad) / 100;
+  
+    if(this.tipo === "basico"){
+      cantidad *= 1.30;
+    }else{
+      cantidad *= 1.50;
+    }
+    
+    return cantidad;
+  }
+
 }
 
-function Interfaz(){
+class Interfaz{
 
+  mostrarError(error, tipo){
+    const div = document.createElement('div');
+    if(tipo === "error"){
+      div.classList = "error";
+    }else{
+      div.classList = "correcto";    
+  
+    }
+    div.innerHTML = error;
+    formulario.insertBefore(div, document.querySelector(".form-group"));  
+  }
+
+  mostrarResumen(seguro, cantidad){
+
+    const resultado = document.getElementById('resultado');
+    const cargando = document.querySelector('#cargando img');
+    let marca;
+  
+    switch(seguro.marca){
+      case '1': 
+      marca = 'Americano';
+      break;
+      case '2': 
+      marca = 'Asiático';
+      break;
+      case '3': 
+      marca = 'Europeo';
+      break;
+    }
+  
+    const div = document.createElement('div');
+    const resumen = `
+      <p>Marca: ${marca}</p>
+      <p>Año: ${seguro.anio}</p>
+      <p>Tipo: ${seguro.tipo}</p>
+      <p>Total: ${cantidad} &euro;</p>
+    `
+    div.innerHTML = resumen;
+    cargando.style.display = 'block';
+    setTimeout(function(){
+      resultado.appendChild(div);
+      cargando.style.display = 'none';
+    }, 3000)
+  }
 }
-
 /** select with 20 last years */
 const selectAnios = document.getElementById('anio');
 
 const max = new Date().getFullYear(),
       min = max - 20;
-
-      console.log(max)
 
 for (let i = max; i > min; i--){
   let option = document.createElement('option');
@@ -23,9 +102,9 @@ for (let i = max; i > min; i--){
   selectAnios.appendChild(option);
 }
 
-const formularioSeguro = document.getElementById('cotizar-seguro');
+const formulario = document.getElementById('cotizar-seguro');
 
-formularioSeguro.addEventListener('submit', function(e){
+formulario.addEventListener('submit', function(e){
   e.preventDefault();
   
   const marca = document.getElementById('marca');
@@ -37,11 +116,18 @@ formularioSeguro.addEventListener('submit', function(e){
   const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
   const interfaz = new Interfaz;
+  const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo )
 
   if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === ''){
-    console.log('obligatorio rellenar todos los datos')
+    interfaz.mostrarError('Es obligatorio rellenar todos los campos', 'error')
   }else{
-    console.log('datos correctos');
+    const resultado = document.querySelector('#resultado div');
+    if(resultado !== null){
+      resultado.remove();
+    }
+
+    const cantidad = seguro.cotizarSeguro()
+    interfaz.mostrarResumen(seguro, cantidad)
   }
 })
 
