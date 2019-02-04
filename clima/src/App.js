82e39f24460f5de89835cb89958b103aaecb1015
jmocana <telecomu4@gmail.com>
2019-02-04 10:13:47 +0100

    Created Header and Formulario component. Validations.

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Formulario from './componentes/Formulario';
 
 class App extends Component {
+
+  state = {
+    error: false
+  }
+
+  consultarDatos = (respuesta) => {
+    if(respuesta.ciudad === '' || respuesta.pais === ''){
+      this.setState({
+        error: true
+      })
+    }else{
+      this.setState({
+        error: false
+      })
+    }
+  }
+
   render() {
     return (
-      <p>Mi APP de clima</p>
+      <div className="App">
+        <Header />
+        <Formulario consultarDatos={this.consultarDatos} />
+      </div>
     );
   }
 }
